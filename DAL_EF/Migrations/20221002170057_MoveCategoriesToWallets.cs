88ddcfd22c37f841bc1f118d22949dcecd3a8049
Ivan Hudzinskyi <ivan.h@shido.com>
2022-10-02 20:01:55 +0300

    add migration

---
+++ DAL_EF/Migrations/20221002170057_MoveCategoriesToWallets.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL_EF.Migrations
+{
+    public partial class MoveCategoriesToWallets : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Users_UserId",
+                table: "Categories");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Categories",
+                newName: "WalletId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Categories_UserId",
+                table: "Categories",
+                newName: "IX_Categories_WalletId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Wallets_WalletId",
+                table: "Categories",
+                column: "WalletId",
+                principalTable: "Wallets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Wallets_WalletId",
+                table: "Categories");
+
+            migrationBuilder.RenameColumn(
+                name: "WalletId",
+                table: "Categories",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Categories_WalletId",
+                table: "Categories",
+                newName: "IX_Categories_UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Users_UserId",
+                table: "Categories",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
